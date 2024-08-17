ea480dc6fc7d175abbd4d0b2d1e3496cb19920e1
Сергей <sergey.vasilev.98@mail.ru>
2024-08-17 20:35:51 +0300

    Create database and tables for companies and vacancies

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,4 @@
+import psycopg2
 import requests
 
 employer_ids = [9694561, 4219, 78638, 80, 15478, 1057, 2748, 1429999, 4181, 1473866]
@@ -23,9 +24,51 @@ def get_vacancies_data():
     :return: список вакансий
     """
     vacancy = []
-    for vacacies_id in employer_ids:
-        url_vac = f"https://api.hh.ru/vacancies?employer_id={vacacies_id}"
+    for vacancies_id in employer_ids:
+        url_vac = f"https://api.hh.ru/vacancies?employer_id={vacancies_id}"
         vacancy_info = requests.get(url_vac, params={'page': 0, 'per_page': 100}).json()
         vacancy.extend(vacancy_info['items'])
     return vacancy
+
+
+def create_database(database_name: str, params: dict) -> None:
+    """
+    функция для создания Базы Данных и создания таблиц в БД
+    """
+    conn = psycopg2.connect(dbname='postgres', **params)
+    conn.autocommit = True
+    cur = conn.cursor()
+
+    cur.execute(f'DROP DATABASE IF EXISTS {database_name}')
+    cur.execute(f'CREATE DATABASE {database_name}')
+
+    conn.close()
+
+    conn = psycopg2.connect(dbname=database_name, **params)
+
+    with conn.cursor() as cur:
+        cur.execute("""
+            CREATE TABLE employers (
+                employer_id INTEGER,
+                employer_name text not null,
+                employer_area TEXT not null,
+                url TEXT,
+                open_vacancies INTEGER
+            )
+        """)
+
+    with conn.cursor() as cur:
+        cur.execute("""
+            CREATE TABLE vacancy (
+                vacancy_id INTEGER,
+                vacancy_name VARCHAR,
+                vacancy_area VARCHAR,
+                salary INTEGER,
+                employer_id INTEGER,
+                vacancy_url VARCHAR
+            )
+        """)
+
+    conn.commit()
+    conn.close()
 
