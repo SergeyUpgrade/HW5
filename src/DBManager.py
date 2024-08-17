44ec4eab48dc32f1de0731c73856c51744e9f5ac
Сергей <sergey.vasilev.98@mail.ru>
2024-08-17 20:40:43 +0300

    Create function get_companies_and_vacancies_count

--- src/DBManager.py
+++ src/DBManager.py
@@ -7,3 +7,17 @@ class DBManager:
     def __init__(self, params):
         self.conn = psycopg2.connect(dbname='hh', **params)
         self.cur = self.conn.cursor()
+
+    def get_companies_and_vacancies_count(self):
+        """
+        получает список всех компаний и количество вакансий у каждой компании.
+        """
+        self.cur.execute("""
+        SELECT employers.employer_name, COUNT(vacancy.employer_id)
+        FROM employers
+        JOIN vacancy USING (employer_id)
+        GROUP BY employers.employer_name
+        ORDER BY COUNT DESC
+        """)
+
+        return self.cur.fetchall()
